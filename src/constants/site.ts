322319690515645cf03a6605c30bf0eb9865f44a
EthanYuan <heyuanshi.dev@outlook.com>
2026-06-23 09:18:01 +0800

    更换主页背景

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -1,1 +1,1 @@
-export const HERO_BACKGROUND_URL = 'https://yuluo-picture-1383397986.cos.ap-guangzhou.myqcloud.com/github_io.webp';
+export const HERO_BACKGROUND_URL = 'https://yuluo-picture-1383397986.cos.ap-guangzhou.myqcloud.com/Album_default_cover/default_cover.webp';
